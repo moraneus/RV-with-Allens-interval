d48d1c00f1a5be02e25901ad5abed10c62c3498a
moraneus <48603901+moraneus@users.noreply.github.com>
2020-09-16 00:00:14 +0300

    Update intervals.pl

--- intervals.pl
+++ intervals.pl
@@ -1,4 +1,3 @@
-%Before and After examples:
 start(y, 14).
 start(v, 5).
 start(a, 10).
@@ -13,14 +12,10 @@ start(z, 1).
 start(d, 20).
 end(d, 25).
 end(z, 3).
-
-%Equal examples:
 start(f, 10).
 start(v, 10).
 end(v, 25).
 end(f, 25).
-
-%Finishes and finish by examples:
 start(j, 17).
 start(n, 15).
 end(n, 20).
@@ -29,14 +24,10 @@ start(m, 10).
 end(j, 20).
 end(m, 20).
 end(k, 20).
-
-%Starts and started by
 start(i, 10).
 end(i, 45).
 start(l, 10).
 end(l, 12.5).
-
-%During and contains example:
 start(g, 11).
 end(g, 14).
 start(q, 34).
@@ -45,7 +36,6 @@ end(r, 45).
 end(q, 57).
 start(e, 1).
 end(e, 14.1).
-
 start(w, 57).
 start(u, 58).
 end(w, 60).
