25ee2b98409df8c35c291b8ea01f86764490edfb
moraneus <48603901+moraneus@users.noreply.github.com>
2020-09-15 23:57:02 +0300

    Add files via upload

---
+++ intervals.pl
@@ -0,0 +1,54 @@
+%Before and After examples:
+start(y, 14).
+start(v, 5).
+start(a, 10).
+start(b, 20).
+end(a, 19).
+start(c, 28).
+end(y, 16).
+end(c, 40).
+end(v, 10).
+end(b, 29).
+start(z, 1).
+start(d, 20).
+end(d, 25).
+end(z, 3).
+
+%Equal examples:
+start(f, 10).
+start(v, 10).
+end(v, 25).
+end(f, 25).
+
+%Finishes and finish by examples:
+start(j, 17).
+start(n, 15).
+end(n, 20).
+start(k, 12).
+start(m, 10).
+end(j, 20).
+end(m, 20).
+end(k, 20).
+
+%Starts and started by
+start(i, 10).
+end(i, 45).
+start(l, 10).
+end(l, 12.5).
+
+%During and contains example:
+start(g, 11).
+end(g, 14).
+start(q, 34).
+start(r, 40).
+end(r, 45).
+end(q, 57).
+start(e, 1).
+end(e, 14.1).
+
+start(w, 57).
+start(u, 58).
+end(w, 60).
+start(o, 59).
+end(o, 62).
+end(u, 61).
